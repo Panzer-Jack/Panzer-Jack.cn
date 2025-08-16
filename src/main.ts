b463031a4dbc88ed6177da35d5191cb543fdf014
Panzer_Jack <danny123456123@126.com>
2025-08-16 22:57:41 +0800

    feat: icon

--- src/main.ts
+++ src/main.ts
@@ -10,20 +10,4 @@ export const createApp = ViteSSG(App, {
   routes,
 }, ({ router, app }) => {
   app.use(createPinia())
-
-  // const html = document.querySelector('html')!
-  // setupRouterScroller(router, {
-  //   selectors: {
-  //     html(ctx) {
-  //       // only do the sliding transition when the scroll position is not 0
-  //       // Disable sliding transition on Dev Mode
-  //       if (ctx.savedPosition?.top || import.meta.hot)
-  //         html.classList.add('no-sliding')
-  //       else
-  //         html.classList.remove('no-sliding')
-  //       return true
-  //     },
-  //   },
-  //   behavior: 'auto',
-  // })
 })
