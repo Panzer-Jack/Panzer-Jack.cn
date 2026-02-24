a0dacc95bea605814009c2e853abe6c245c61b67
老船长PZ.Jack <panzer_jack@panzer-jack.cn>
2026-02-24 11:38:50 +0800

    feat: new friends

--- src/data/links.ts
+++ src/data/links.ts
@@ -6,15 +6,21 @@ import type { FriendLink } from '@/types/link'
  */
 export const friendLinks: FriendLink[] = [
   {
-    name: '老船长PZ_Jack',
-    link: 'https://blog.panzer-jack.cn',
-    description: 'A Pragmatistic Dreamer.',
-    avatar: 'https://blog.panzer-jack.cn/avatar.png',
-  },
-  {
     name: '余弦の博客',
     link: 'https://blog.cosine.ren',
     description: 'FE / ACG / 手工 / 深色模式强迫症 / INFP / 兴趣广泛养两只猫的老宅女 / remote ',
     avatar: 'https://blog.cosine.ren/img/avatar.webp',
+  },
+  {
+    name: 'SuemorのBlog',
+    link: 'https://www.suemor.com',
+    description: '在探索的过程中遇见更好的自己',
+    avatar: 'https://y.suemor.com/suemor-avatar.jpeg',
+  },
+  {
+    name: 'Richard\'s Docs',
+    link: 'https://richard-docs.netlify.app/',
+    description: 'Don\'t just daydream, go do it',
+    avatar: 'https://github.com/Richard-Zhang1019.png',
   },
 ]
